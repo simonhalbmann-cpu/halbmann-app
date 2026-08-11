6edc20d741a9032a50e5a0c32a3c696b2640e84b
Simon Halbmann <simon@example.com>
2026-08-11 13:17:33 +0200

    Remove Google font dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata, Viewport } from 'next';
-import { Cormorant_Garamond, Manrope } from 'next/font/google';
 import { AuthProvider } from '../context/AuthContext';
 import './globals.css';
-
-const displayFont = Cormorant_Garamond({
-  subsets: ['latin'],
-  variable: '--font-display',
-  weight: ['500', '600', '700'],
-});
-
-const bodyFont = Manrope({
-  subsets: ['latin'],
-  variable: '--font-body',
-});
 
 export const metadata: Metadata = {
   title: 'Halbmann Holding',
@@ -30,10 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      className={`${displayFont.variable} ${bodyFont.variable}`}
-      lang="de"
-    >
+    <html lang="de">
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
